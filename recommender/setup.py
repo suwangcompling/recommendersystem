3bb3101a7160e81100614c3a7f64c8c7ac4dcc68
suwangcompling <shrekwang@utexas.edu>
2016-06-27 20:45:09 -0500

    Create setup.py

---
+++ recommender/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='recommender',
+      version='0.1',
+      description='first_recommender_system',
+      url='https://github.com/suwangcompling',
+      author='jacob su',
+      author_email='shrekwang1985@gmail.com',
+      license='GNU',
+      packages=['recommender'],
+      zip_safe=False)
